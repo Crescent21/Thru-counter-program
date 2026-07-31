c2b2f21ff5e142fb53e7292feffd70b783fdeb19
Toyosi Omotunde <omotundetoyosi33@gmail.com>
2026-07-31 11:22:53 +0100

    Add SET instruction, underflow/overflow protection, and voting DAO program

--- examples/tn_counter_program.c
+++ examples/tn_counter_program.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <limits.h>
 #include <thru-sdk/c/tn_sdk.h>
 #include <thru-sdk/c/tn_sdk_syscall.h>
 #include "tn_counter_program.h"
@@ -66,12 +67,17 @@ static void handle_decrement_counter(
     }
 
     /* Decrement the counter (don't go below zero) */
-tn_counter_account_t *counter_account =
-    (tn_counter_account_t *)account_data;
+    tn_counter_account_t *counter_account =
+        (tn_counter_account_t *)account_data;
 
-if (counter_account->counter_value > 0) {
+    if (counter_account->counter_value == 0) {
+        tsdk_return(TN_COUNTER_ERR_COUNTER_UNDERFLOW);
+    }
+
     counter_account->counter_value--;
-}
+
+
+
 
 /* Emit decrement event */
 tsys_emit_event(
@@ -116,6 +122,42 @@ static void handle_reset_counter(
 
     tsdk_return(TSDK_SUCCESS);
 }
+
+static void handle_set_counter(
+    uchar const *instruction_data,
+    ulong instruction_data_sz TSDK_PARAM_UNUSED
+) {
+    tn_counter_set_args_t const *args =
+        (tn_counter_set_args_t const *)instruction_data;
+
+    ushort account_idx = args->account_index;
+    ulong new_value = args->new_value;
+
+    /* Get account data pointer */
+    void *account_data = tsdk_get_account_data_ptr(account_idx);
+    if (account_data == NULL) {
+        tsdk_revert(TN_COUNTER_ERR_ACCOUNT_DATA_ACCESS_FAILED);
+    }
+
+    /* Set account as writable */
+    ulong result = tsys_set_account_data_writable(account_idx);
+    if (result != TSDK_SUCCESS) {
+        tsdk_revert(TN_COUNTER_ERR_ACCOUNT_SET_WRITABLE_FAILED);
+    }
+
+    /* Set counter to the requested value */
+    tn_counter_account_t *counter_account =
+        (tn_counter_account_t *)account_data;
+    counter_account->counter_value = new_value;
+
+    /* Emit set event */
+    tsys_emit_event(
+        (uchar const *)&counter_account->counter_value,
+        sizeof(ulong)
+    );
+
+    tsdk_return(TSDK_SUCCESS);
+}
 static void handle_increment_counter(uchar const *instruction_data, ulong instruction_data_sz TSDK_PARAM_UNUSED) {
     tn_counter_increment_args_t const *args = (tn_counter_increment_args_t const *)instruction_data;
 
@@ -133,8 +175,14 @@ static void handle_increment_counter(uchar const *instruction_data, ulong instru
         tsdk_revert(TN_COUNTER_ERR_ACCOUNT_SET_WRITABLE_FAILED);
     }
 
-    /* Increment the counter */
+    
+    /* Increment the counter (prevent overflow) */
     tn_counter_account_t* counter_account = (tn_counter_account_t*)account_data;
+
+    if (counter_account->counter_value == ULONG_MAX) {
+        tsdk_return(TN_COUNTER_ERR_COUNTER_OVERFLOW);
+    }
+
     counter_account->counter_value++;
 
     /* Emit increment event - emit just the counter value */
@@ -158,22 +206,22 @@ TSDK_ENTRYPOINT_FN void start(void) {
      switch (*instruction_type) {
 
 case TN_COUNTER_INSTRUCTION_CREATE:
-    if (instruction_data_sz < sizeof(tn_counter_create_args_t)) {
-        tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
-    }
+        if (instruction_data_sz < sizeof(tn_counter_create_args_t)) {
+            tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
+        }
 
-    tn_counter_create_args_t const *create_args =
-        (tn_counter_create_args_t const *)instruction_data;
+        tn_counter_create_args_t const *create_args =
+            (tn_counter_create_args_t const *)instruction_data;
 
-    ulong expected_size =
-        sizeof(tn_counter_create_args_t) + create_args->proof_size;
+        ulong expected_size =
+            sizeof(tn_counter_create_args_t) + create_args->proof_size;
 
-    if (instruction_data_sz != expected_size) {
-        tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
-    }
+        if (instruction_data_sz != expected_size) {
+            tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
+        }
 
-    handle_create_counter(instruction_data, instruction_data_sz);
-    break;
+        handle_create_counter(instruction_data, instruction_data_sz);
+        break;
 
 case TN_COUNTER_INSTRUCTION_INCREMENT:
     if (instruction_data_sz != sizeof(tn_counter_increment_args_t)) {
@@ -192,15 +240,23 @@ case TN_COUNTER_INSTRUCTION_DECREMENT:
     break;
 
 case TN_COUNTER_INSTRUCTION_RESET:
-    if (instruction_data_sz != sizeof(tn_counter_increment_args_t)) {
-        tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
+        if (instruction_data_sz != sizeof(tn_counter_increment_args_t)) {
+            tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
+        }
+
+        handle_reset_counter(instruction_data, instruction_data_sz);
+        break;
+    case TN_COUNTER_INSTRUCTION_SET:
+        if (instruction_data_sz != sizeof(tn_counter_set_args_t)) {
+            tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_DATA_SIZE);
+        }
+
+        handle_set_counter(instruction_data, instruction_data_sz);
+        break;
+    default:
+        tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_TYPE);
     }
 
-    handle_reset_counter(instruction_data, instruction_data_sz);
-    break;
-default:
-    tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_TYPE);
-}
 
     /* Should never reach here */
     tsdk_revert(TN_COUNTER_ERR_INVALID_INSTRUCTION_TYPE);
---
+++ examples/voting/Local.mk
@@ -0,0 +1,2 @@
+# Voting DAO Program
+$(call make-bin,tn_voting_program_c,tn_voting_program,,-ltn_sdk)
